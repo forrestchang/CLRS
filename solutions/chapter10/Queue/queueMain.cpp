35a09f92fcb74a832b53f2c511c55c424f81687e
Jiayuan Zhang <forrestchang7@gmail.com>
2015-10-31 18:17:41 +0800

    update queueMain.cpp

--- solutions/chapter10/Queue/queueMain.cpp
+++ solutions/chapter10/Queue/queueMain.cpp
@@ -7,15 +7,7 @@ using namespace std;
 int main(void) {
   Queue queue(10);
 
-  for (int i = 0; i < 9; i++) {
-    queue.enqueue(i);
-  }
-
-  for (int i = 0; i < 9; i++) {
-    cout << queue.dequeue() << " ";
-  }
-
-  cout << endl;
+  
 
   return 0;
 }
